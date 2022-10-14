44933e6a1595883ceda70f0a36984029c263e9b0
misung Kim <mizzkkim7@gmail.com>
2022-10-14 19:55:06 +0900

    FIX:배포에러:BrowserRouter삭제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 
 import SignUp from './pages/Auth/SignUp';
 import Todo from './pages/Todo/Todo';
@@ -8,12 +8,10 @@ import Todo from './pages/Todo/Todo';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignUp />}></Route>
           <Route path="/todo" element={<Todo />}></Route>
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
