0b97bc3b7c41639492f5fb0d2f3a94a4c31d71bd
misung Kim <mizzkkim7@gmail.com>
2022-10-14 20:03:42 +0900

    FIX:App.js -> basename 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {  Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import SignUp from './pages/Auth/SignUp';
 import Todo from './pages/Todo/Todo';
@@ -8,10 +8,12 @@ import Todo from './pages/Todo/Todo';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<SignUp />}></Route>
           <Route path="/todo" element={<Todo />}></Route>
         </Routes>
+      </BrowserRouter>
     </div>
   );
 }
